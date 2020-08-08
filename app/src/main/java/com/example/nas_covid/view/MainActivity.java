876e920dafcffda21003e260c7aaa3f5b69936fe
Saleh Sharqan <salehs.j2020@gmail.com>
2020-08-08 16:20:01 +0800

    MVP Structure2

--- app/src/main/java/com/example/nas_covid/view/MainActivity.java
+++ app/src/main/java/com/example/nas_covid/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.nas_covid;
+package com.example.nas_covid.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.nas_covid.R;
 
 public class MainActivity extends AppCompatActivity {
 
